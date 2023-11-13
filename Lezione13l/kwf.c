a3f3941994cd35024c75f4dc440fb562bb51e8e3
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-11-13 15:32:14 +0100

    finito es.1 processi

--- Lezione13l/kwf.c
+++ Lezione13l/kwf.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    
+    FILE *fp;
+    int kids, writes;
+    if(argc < 4)
+    {
+        fprintf(stderr, "Not Enough Arguments");
+        return -1;
+    }
+    if((fp=fopen(argv[1],"w"))==NULL)
+    {
+        fprintf(stderr, "Error opening file");
+        return -1;
+    }
+    if(!(kids=atoi(argv[2])) || !(writes=atoi(argv[3])) || kids<0 || writes<0)
+    {
+        fprintf(stderr, "Argument Error");
+        return -1;
+    }
+    for(int i=0;i<kids;i++)
+    {
+        if(!fork())
+        {
+            for(int j=0;j<writes;j++)
+                fprintf(fp,"PID: %i\tPPID: %i\n",getpid(),getppid());
+            exit(0);
+        }
+            
+        
+    }
+    fclose(fp);
+    return 0;
+
+
+
+
 }
